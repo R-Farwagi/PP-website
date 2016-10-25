535fc163d302b64b2bd6febc47ed07a485d63d4f
R-Farwagi <rajaifarwagi@gmail.com>
2016-10-25 23:50:55 +0300

    Add files via upload

--- input.py
+++ input.py
@@ -2,15 +2,16 @@ import datetime
 date= str(datetime.datetime.now())
 print (date)
 print("enter three action to schedule your day!")
-
+def time():
+    date = time1
+    return time
 hours= str(time[0:2])
-minutes= str(time[4:5]
+minutes= str(time[4:5])
 
              
-for i in range (hours):
+for i in range (0,5):
     action = input()
     time2 = time1 + int(input("how much time (in hours) would you like to allocate for this action? "))
     print ("you will have " + str(action) + "  " + "from " + str(time1) + " " + "until " + str(time2) + " .Is there anything else you would like to add to your schedule?")
     time1 = time2
-    i += 1
-    
+
